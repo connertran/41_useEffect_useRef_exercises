96f6fad36afa7df045aa350d7ffdfe2a19408723
Conner Tran <conner.tran@bsa.edu.vn>
2024-05-12 14:51:19 +0200

    finshed the card game app

--- src/GameArea.js
+++ src/GameArea.js
@@ -6,6 +6,7 @@ const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
 const GameArea = () => {
   const [deck, setDeck] = useState(null);
   const [drawn, setDrawn] = useState([]);
+  const [isShuffling, setIsShuffling] = useState(false);
 
   // this function only run once when the page is loaded or refreshed because useEffect is used and the [] is empty
   useEffect(function initDeckFromApi() {
@@ -39,9 +40,24 @@ const GameArea = () => {
       alert(`${err}`);
     }
   }
+
+  async function shuffleDeck() {
+    setIsShuffling(true);
+    try {
+      await axios.get(`${API_BASE_URL}/${deck.deck_id}/shuffle/`);
+      setDrawn([]);
+    } catch (e) {
+      alert(e);
+    } finally {
+      setIsShuffling(false);
+    }
+  }
   return (
     <div className="GameArea">
       <button onClick={drawACard}>GIMME A CARD!</button>
+      {isShuffling === false && (
+        <button onClick={shuffleDeck}>Shuffle the Deck</button>
+      )}
       <div>
         {drawn.map((c) => (
           <Card key={c.id} name={c.name} image={c.image} />
